37f7d1a87e563edd03f16711930cba921c51e01a
ismael junior <ifsjunior2@gmail.com>
2022-11-11 20:47:55 -0300

    feat(rotas) inclusão de novas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Cadastro from "../views/Cadastro";
 import Newpage from "../views/Qrcode/qrcode";
 import Formcadastro from "../views/Formcadastro/index";
+import Consultacadastro from '../views/Consultacadastro/index';
 
 export default function Rotas() {
     return(
@@ -11,6 +12,7 @@ export default function Rotas() {
                 <Route path="/cadastro" element={ <Cadastro />} />
                 <Route path="/Newpage" element={ <Newpage />} /> 
                 <Route path="/Newcad" element={ <Formcadastro />} /> 
+                <Route path="/consultacadastro" element={ <Consultacadastro />} /> 
             </Routes>
     );
 }
